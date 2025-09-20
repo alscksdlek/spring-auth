f22f68d7201caf4ac8e179fa4e88919b024dd613
alscksdlek <mseon916@naver.com>
2025-09-20 22:34:25 +0900

    forbidden page

--- src/main/java/com/sparta/spring_auth/controller/ProductController.java
+++ src/main/java/com/sparta/spring_auth/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.sparta.spring_auth.controller;
 
 import com.sparta.spring_auth.entity.User;
+import com.sparta.spring_auth.entity.UserRoleEnum;
+import com.sparta.spring_auth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("user");
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetials) {
+        // Authentication의 Principle
+        User user = userDetials.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
+        System.out.println("user.getEmail() = " + user.getEmail());
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
