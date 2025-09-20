43630bcea2553a177f7b5571c32ce169d64dbfb6
alscksdlek <mseon916@naver.com>
2025-09-20 22:57:35 +0900

    validation exception

--- src/main/java/com/sparta/spring_auth/controller/UserController.java
+++ src/main/java/com/sparta/spring_auth/controller/UserController.java
@@ -3,18 +3,24 @@ package com.sparta.spring_auth.controller;
 import com.sparta.spring_auth.dto.LoginRequestDto;
 import com.sparta.spring_auth.dto.SignupRequestDto;
 import com.sparta.spring_auth.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -31,12 +37,28 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) { //BindingResult: 오류에 대한 정보 담겨 들어옴
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
     }
 
+    //    @PostMapping("/user/signup")
+//    public String signup(SignupRequestDto requestDto) {
+//        userService.signup(requestDto);
+//
+//        return "redirect:/api/user/login-page";
+//    }
+//
 //    @PostMapping("/user/login")
 //    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
 //        try {
